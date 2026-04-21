52d9f75f7616e41200e3ecbcee1821ceb418714a
YoshiRodriguez <munikaeru@gmail.com>
2026-04-21 14:56:42 -0600

    feat: new timer design; controls now working as well

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -24,12 +24,36 @@ function Timer() {
 
     )
 
+    const parsedMinutes = Math.floor(seconds / 60);
+    const parsedSeconds = seconds % 60;
+
+
     return (
         <div className="ds-timer">
-            <h3>Expected time:</h3>
+            {/* <h3>Expected time:</h3>
             <h1 style={{ margin: "0.1rem 0" }}>
                 {seconds}<span style={{ fontSize: "1.5rem" }}>s</span> <span style={{ fontSize: "1rem", color: "var(--ds-text-muted)" }}> (Status: {isRunning ? "Running" : "Paused"})</span>
-            </h1>
+            </h1> */}
+
+            <div className="countdown-timer">
+                <div className="countdown-segment">
+                    <span className="countdown-number">
+                        {parsedMinutes.toString().padStart(2, '0')}
+                    </span>
+                    <span className="countdown-unit">Minutes</span>
+                </div>
+
+                <div className="countdown-segment">
+                    <span className="countdown-number">
+                        {parsedSeconds.toString().padStart(2, '0')}
+                    </span>
+                    <span className="countdown-unit">Seconds</span>
+                </div>
+            </div>
+
+            <p style={{ textAlign: "center", color: "var(--ds-text-muted)" }}>
+                Status: {isRunning ? "Running" : "Paused"}
+            </p>
 
 
             <div className="ds-timer-controls">
